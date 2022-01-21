935d442c03decd2d52cc54b17831eb7f827ea4ad
Techncrypt <techncrypt@gmail.com>
2022-01-20 22:12:44 -0700

    Added rounds loop

--- player.py
+++ player.py
@@ -4,6 +4,13 @@ class Player:
     def __init__(self):
         pass
 
+
+
+    def start_gameg(self):    
+        """while loop to cycle through future game stuff"""
+        while self.rounds:
+            pass
+
     def current_card(self):
         """Gets the assigned card"""
         self.card.current_card
