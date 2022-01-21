e83805dee1d514e0fc865563303e8f624aa9f560
Techncrypt <techncrypt@gmail.com>
2022-01-20 22:04:40 -0700

    Added some ideas to the cards class

--- cards.py
+++ cards.py
@@ -1,3 +1,27 @@
+import random
+
 class Cards:
+
     def __init__(self):
-        pass
+        self.current_card = 0
+        self.next_card = 0
+
+    def card(self):
+        """Card will choose a random number from 1-13."""
+        self.card_number1 = random.randint(1, 13)
+        
+    
+    def highpoints(self):
+        """Points will adjust based comparison to the current card"""
+        if self.next_card > self.current_card:
+            return +100
+        elif self.next_card < self.current_card:
+            return -75
+
+
+    def lowpoints(self):
+        """Points will adjust based comparison to the current card"""
+        if self.next_card < self.current_card:
+            return self.pointstotal + 100
+        elif self.next_card > self.current_card:
+            return self.pointstotal - 75
