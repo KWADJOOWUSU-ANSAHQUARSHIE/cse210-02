091e9e68f4827d42c7bfc1d311f4e38c05d4b4ce
seth-godoy <u443447760@byui.edu>
2022-01-22 17:22:51 -0600

    Player fix

--- player.py
+++ player.py
@@ -12,7 +12,7 @@ class Player:
         self.guess = ''
 
         # Make an instance of the class Cards so we can use it whenever we want.
-        card = Cards()
+        self.card = Cards()
 
         # Two different variables we can use to draw cards using the instance created above.
         # We draw current first and then we draw next to compare.
@@ -23,8 +23,13 @@ class Player:
         # Call the first_guess function, then write the while loop.
         self.first_guess()
         while self.is_playing:
-            self.make_guess()
-            self.is_playing
+            self.check_guess()
+            if self.is_playing:
+                self.display_output()
+                if self.is_playing:
+                    self.keep_playing()
+                    if self.is_playing:
+                        self.make_guess()
 
     def first_guess(self):
         # Use this function to draw and display the first current card and ask the user for the guess.
@@ -34,78 +39,76 @@ class Player:
         # we change self.next to a new card. If we define the first self.current inside the while
         # loop, the self.current = self.next will be overwritten every time the while loop starts again.
         print()
-        print("Welcome player!")
-        print()
-        print("Your current score is 300 points. Based on your guess you will loose points or earn points. When you reach 0 the game will be over.")
-        print()
-        card = Cards()
-        card.draw_card()
-        self.current = card.draw
-        print(f"You are holding a {self.current}")
-        self.guess=input("Will the next card be higher or lower? (h/l): ")
-        self.check_guess()
+        print("Welcome player!\n")
+        print("Your current score is 300 points.")
+        print("Based on your guess you will lose points or earn points.")
+        print("If the card is the same, you will automatically earn 100 points.")
+        print("When you reach 0, the game will be over.\n")
+
+        self.card.draw_card()
+        self.current = self.card.draw
+
+        print(f"You are holding a {self.current}.")
+        self.guess = input("Will the next card be higher or lower? (h/l): ")
 
     def make_guess(self):
         # Use this function to display the current card and ask the user for the guess.
         # This function goes inside the while loop.
-        print()
-        print(f"You are holding a {self.next}")
-        self.guess=input("Will the next card be higher or lower? (h/l): ")
-        self.check_guess()
+        print(f"You are holding a {self.current}.")
+        self.guess = input("Will the next card be higher or lower? (h/l): ")
 
     def check_guess(self):
         # Draw a card for self.next and check if the guess is correct or not.
-        card = Cards()
-        card.draw_card()
-        self.next = card.draw
+        self.card.draw_card()
+        self.next = self.card.draw
 
-        if self.next >= self.current:
-            answer = "higher"
-        elif self.next <= self.current:
-            answer = "lower"
+        if self.guess == "h":
+            if self.current > self.next:
+                self.score -= 75
+            else:
+                self.score += 100
 
-        if self.guess == "h" and answer == "higher":
-            self.score += 100
-        elif self.guess == "l" and answer == "lower":
-            self.score += 100
-        elif self.guess == "h" and answer == "lower":
-            self.score -= 75
-        elif self.guess == "l" and answer == "higher":
-            self.score -= 75
-        
-        print(f"The next card was a {self.next}")
-        self.display_output()
-        self.keep_playing()
+        elif self.guess == "l":
+            if self.current < self.next:
+                self.score -= 75
+            else:
+                self.score += 100
+
+        else:
+            print()
+            print("Entered an invalid input.\nThe game is over.")
+            print("Follow the rules next time >:(\n")
+            self.is_playing = False
 
     def display_output(self):
         # Display the score after the guess and if score < 0, self.is_playing = False to end game.
         # If game continues, make the change self.current = self.next.
         # Then, draw a new card in self.next.
-        if self.score > 0:
-            print(f"Your current score is {self.score}")
-            # self.current = self.next
-            # card = Cards()
-            # card.draw_card()
-            # self.next = card.draw
+        print(f"The next card was a {self.next}.")
 
-        elif self.score <= 0:
+        if self.score <= 0:
+            print(f"Your current score is {self.score}.\n")
+            print("Sorry! you lost.\nTry again next time.\n")
             self.is_playing = False
-            print("Sorry you lost, try again next time")
-            
+
+        else:
+            print(f"Your current score is {self.score}")
+
+            self.current = self.next
+            self.card.draw_card()
+            self.next = self.card.draw
 
     def keep_playing(self):
-        
         # Depending of the answer, change self.is_playing to False to end loop.
-        play_more = None
-        while play_more not in ("yes", "no"): 
-            
-            play_more = input("Would you like to keep playing (yes or no)?: ")
-            if play_more == "yes":
-                self.is_playing = True
-                self.make_guess()
-            elif play_more == "no":
-                print("Thanks for Playing :)")
-                self.is_playing = False
-            else: 
-                print("Please enter yes or no.")
-    
+        play_more = input("Would you like to keep playing? [yes/no] ")
+
+        if play_more == "yes":
+            print()
+        elif play_more == "no":
+            print("\nThanks for Playing :)\n")
+            self.is_playing = False
+        else:
+            print()
+            print("Entered an invalid input.\nThe game is over.")
+            print("Follow the rules next time >:(\n")
+            self.is_playing = False
